7afbf30f5ddf611b2bd880e10cafd4b61b12bb63
Matt Kubilus <mattkubilus@gmail.com>
2018-02-16 20:17:05 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
-apt-get install -y lame eject id3 id3v2 eyed3 normalize-audio vorbisgain mkcue mp3gain libdata-dump-perl flac pmount glyrc abcde
+#!/bin/sh
+
+apt-get install -y lame eject id3 id3v2 eyed3 normalize-audio vorbisgain mkcue mp3gain libdata-dump-perl flac glyrc abcde
 cp 99-cd-audio-processing.rules /etc/udev/rules.d/.
 cp ripit.sh /usr/local/bin/.
 cp ripit.service /etc/systemd/system/.
-wget https://abcde.einval.com/download/abcde-2.8.1.tar.gzabcde-2.8.1.tar.gz
-tar xvzf abcde-2.8.1.tar.gz
+#wget https://abcde.einval.com/download/abcde-2.8.1.tar.gzabcde-2.8.1.tar.gz
+#tar xvzf abcde-2.8.1.tar.gz
 cp abcde-2.8.1/abcde /usr/bin/.
 cp abcde-2.8.1/cddb-tool /usr/bin/.
 cp abcde-2.8.1/abcde-musicbrainz-tool /usr/bin/.
 cp abcde.conf /etc/abcde.conf
 udevadm control --reload
+mkdir -p /media/music
--- ripit.sh
+++ ripit.sh
@@ -2,15 +2,37 @@
 LOGFILE=/var/log/cdrip.log
 
 finish() {
-    eject
-    pumount /media/music
+    (
+      echo "Unmounting"
+      umount /media/music
+      sleep 20
+      echo "Done, ejecting CD"
+      eject
+      RET=$?
+      echo "Exit with: $RET"
+    ) >> $LOGFILE 2>&1
 }
 trap finish EXIT
 
 echo "$(date)" >> $LOGFILE
 ( 
-    pmount /dev/sda1 music || pmount /dev/sdb1 music
-    time PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin abcde -GN
+    mount /dev/sda1 /media/music || mount /dev/sdb1 /media/music
+    RET=$?
+    if [ $RET -ne 0 ]; then
+       echo "Failed to mount, exiting."
+       eject
+       exit 1
+    fi
+    MPOINT=$(mount | grep /media/music)
+    echo "Mounted /media/music: $MPOINT"
 
+    time PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin abcde -GN
+    RET=$?
+    if [ $RET -eq 0 ]; then
+        echo "Successfully ripped CD"
+    else
+        echo "Rip failed!"
+    fi
+    sleep 5
 ) >> $LOGFILE 2>&1 
 exit
